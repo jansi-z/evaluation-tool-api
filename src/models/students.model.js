3982f9eaab21d482d5ea06026f6fd78b13f2ff44
Jan-Sijmen <js.zwarts@gmail.com>
2017-10-10 10:49:02 +0200

    Adds students service

---
+++ src/models/students.model.js
@@ -0,0 +1,28 @@
+// students-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+
+  const evaluationSchema = new Schema({
+    color: { type: String, required: true },
+    remark: { type: String },
+    author: { type: Schema.Types.ObjectId, ref: 'users' },
+    date: { type: Date, required: true }
+  });
+
+  const students = new Schema({
+
+    name: { type: String, required: true },
+    photo: { type: String, required: true },
+    class: { type: Schema.Types.ObjectId, ref: 'classes' },
+    evaluations: [evaluationSchema],
+
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+  });
+
+  return mongooseClient.model('students', students);
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const users = require('./users/users.service.js');
 const classes = require('./classes/classes.service.js');
+const students = require('./students/students.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(users);
   app.configure(classes);
+  app.configure(students);
 };
