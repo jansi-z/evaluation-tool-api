5a132a7eca5eda223222ae2f1be6d43aeee511aa
Jan-Sijmen <js.zwarts@gmail.com>
2017-10-10 10:08:47 +0200

    Adds name to user

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -5,11 +5,12 @@
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const users = new mongooseClient.Schema({
-  
-    email: {type: String, unique: true},
+
+    name: { type: String },
+    email: { type: String, unique: true },
     password: { type: String },
-  
-  
+
+
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
