fb0653c0ee812f6916b191b668b0d953b4970c68
Jan-Sijmen <js.zwarts@gmail.com>
2017-10-11 17:07:02 +0200

    Adds evaluations service

---
+++ src/models/evaluations.model.js
@@ -0,0 +1,15 @@
+// evaluations-model.js - A mongoose model
+// 
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+  const evaluations = new Schema({
+    text: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+  });
+
+  return mongooseClient.model('evaluations', evaluations);
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
 const users = require('./users/users.service.js');
 const batches = require('./batches/batches.service.js');
 const students = require('./students/students.service.js');
+const evaluations = require('./evaluations/evaluations.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(users);
   app.configure(batches);
   app.configure(students);
+  app.configure(evaluations);
 };
