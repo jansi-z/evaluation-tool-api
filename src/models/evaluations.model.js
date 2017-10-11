0e6951160a61ec342fe0bfd56138463ab6426cad
Jan-Sijmen <js.zwarts@gmail.com>
2017-10-11 17:16:39 +0200

    Sets up evaluations service and links it to students and users

--- src/models/evaluations.model.js
+++ src/models/evaluations.model.js
@@ -1,12 +1,16 @@
 // evaluations-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
   const evaluations = new Schema({
-    text: { type: String, required: true },
+    studentId: { type: Schema.Types.ObjectId, ref: 'students', required: true },
+    authorId: { type: Schema.Types.ObjectId, ref: 'users', required: true },
+    date: { type: Date },
+    color: { type: String },
+    remark: { type: String },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
--- src/models/students.model.js
+++ src/models/students.model.js
@@ -6,20 +6,12 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
 
-  const evaluationSchema = new Schema({
-    color: { type: String, required: true },
-    remark: { type: String },
-    author: { type: Schema.Types.ObjectId, ref: 'users' },
-    date: { type: Date, required: true }
-  });
-
   const students = new Schema({
 
     name: { type: String, required: true },
     photo: { type: String, required: true },
     batchId: { type: Schema.Types.ObjectId, ref: 'batches' },
-    evaluations: [evaluationSchema],
-
+    evaluationIds: [{ type: Schema.Types.ObjectId, ref: 'evaluations' }],
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
